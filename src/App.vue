9aa7a56fea6b70b21bed54793c01262ae07a19c4
kimjustind <kimjustind@gmail.com>
2024-03-25 20:53:54 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import QRCodeGenerator from './components/qrcodegenerator.vue';
+</script>
+
+<script>
+export default {
+  components: {
+    QRCodeGenerator,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>QR Code Generator</h1>
+    <QRCodeGenerator />
+  </div>
+</template>
---
+++ src/components/qrcodegenerator.vue
@@ -0,0 +1,40 @@
+<script>
+import QrcodeVue from 'qrcode.vue';
+
+export default {
+  components: {
+    QrcodeVue,
+  },
+  data() {
+    return {
+      qrCodeData: '',
+      size: 200,
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="qrdiv">
+    <input v-model="qrCodeData" placeholder="Enter link" />
+    <qrcode-vue id="qrcode" :value="qrCodeData" :size="size" level="Q" />
+  </div>
+</template>
+
+<style>
+  #qrdiv {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+  }
+  input {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 20px;
+    width: 20em;
+    text-align: center;
+  }
+  #qrcode {
+    border: solid #fff 4px;
+  }
+</style>
